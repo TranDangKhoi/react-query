f8317d155e7e130457f0b9f88a3d08b6504f02f6
TranDangKhoi <baits16@gmail.com>
2023-02-13 22:18:51 +0700

    :+1: Using react query to fetch data and display loading

--- src/apis/students.api.ts
+++ src/apis/students.api.ts
@@ -1,8 +1,8 @@
-import { Students } from "types/students.type";
+import { StudentsType } from "types/students.type";
 import http from "utils/http";
 
 export const getStudents = (page: number | string, limit: number | string) =>
-  http.get<Students>("/students", {
+  http.get<StudentsType>("/students", {
     params: {
       _page: page,
       _limit: limit
