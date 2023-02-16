e58a0da9b60ec3cc16e735c8fe7080aaec713a60
TranDangKhoi <baits16@gmail.com>
2023-02-17 00:16:59 +0700

    :+1: Declare new http requests for edit and delete

--- src/apis/students.api.ts
+++ src/apis/students.api.ts
@@ -9,4 +9,11 @@ export const getStudents = (page: number | string, limit: number | string) =>
     },
   });
 
+export const getStudentById = (studentId: number | string) => http.get<StudentType>(`/students/${studentId}`);
+
 export const addStudent = (body: Omit<StudentType, "id">) => http.post<StudentsType>("/students", body);
+
+export const updateStudentById = (studentId: number | string, body: StudentType) =>
+  http.put<StudentType>(`/students/${studentId}`, body);
+
+export const deleteStudentById = (studentId: number | string) => http.delete<{}>(`/students/${studentId}`);
